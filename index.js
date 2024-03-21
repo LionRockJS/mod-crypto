e7b609164a3c02c50c891bd1850b62f8bb2bdf7d
Colin Leung <colin@cowise.co>
2024-03-22 02:48:42 +0800

    chore: update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import url from "node:url";
 const dirname = url.fileURLToPath(new URL('.', import.meta.url)).replace(/\/$/, '');
-export default {dirname}
+export default {
+  dirname
+}
 
 import HelperCrypto from './classes/helper/Crypto';
 
-export{
+export {
   HelperCrypto
 }
